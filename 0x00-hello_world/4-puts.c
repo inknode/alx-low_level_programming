37b4495529472f62f1c679e7f4b2a64b38d9fe2a
Okenwa Ukelonu <oukelonu@gmail.com>
2022-06-30 09:11:18 -0700

    Use puts instead of print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Description: puts "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
