c2c245975870b38bde0b991bc3776c73d0e6f3da
Okenwa Ukelonu <oukelonu@gmail.com>
2022-07-28 19:39:29 -0700

    fix

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,68 +1,47 @@
+#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-/**
-  * _isdigit - tells if the string consists of digits
-  * @argv: pointer to current item in argument
-  * Return: return 0 if all digits, 1 if not all digits.
-  */
-int _isdigit(char *argv)
-{
-	int i;
 
-	i = 0;
-	while (argv[i])
-	{
-		if (argv[i] >= '0' && argv[i] <= '9')
-			i++;
-		else
-			return (1);
-	}
-	return (0);
-}
-/**
-  * _atoi - converts a string of ascii digits to the values they represent
-  * @s: pointer to the source string
-  * Return: value of digits
-  */
-int _atoi(char *s)
-{
-	int i, result;
+#define ERR_MSG "Error"
 
-	i = result = 0;
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		i++;
-	}
-	return (result);
-}
+int _isdigit(char *s);
+
 /**
-  * main - main function call
-  * @argc: argument count
-  * @argv: 2D array of arguments
-  * Return: return 0 on success, 98 on failure
-  */
+ * main - takes two numbers as arguments and outputs the product.
+ * @argc: the number of arguments including name of program.
+ * @argv: an array of arguments
+ *
+ * Return: 0 if successful.
+ */
 int main(int argc, char *argv[])
 {
 	int i;
-
-	malloc();
 	if (argc != 3)
 	{
-		printf("Error\n");
+		for (i = 0; ERR_MSG[i] != '\0'; i++)
+			_putchar(ERR_MSG[i]);
+		_putchar(10);
 		exit(98);
 	}
-	for (i = 1; i < argc; i++)
+	if (_isdigit(argv[1]) == 0)
+		printf("%s\n", argv[1]);
+		
+	return (0);
+}
+
+/**
+ * isdigit - checks if string is a number or not
+ * @s: the string to be checked
+ *
+ * Return: 1 if true, else 0.
+ */
+
+int _isdigit(char *s)
+{
+	while (*s)
 	{
-		if (_isdigit(argv[i]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+		if (*s < 48 || *s > 57)
+			return (1);
+		s++;
 	}
 	return (0);
 }
